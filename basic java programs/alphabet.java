f32d43b23bae79ebbbdfe3f49c51913360acb9e2
dipalimagar123 <magardipali733@gmail.com>
2023-02-28 10:29:31 +0530

    Initial cprogramommit

---
+++ basic java programs/alphabet.java
@@ -0,0 +1,103 @@
+import java.util.Scanner;
+public class alphabet {
+    public static void main(String[] args) {
+        String a;
+    Scanner sc = new Scanner(System.in);
+          a = sc.nextInt();
+          switch a;
+         
+      case "a":
+          System.out.println("this is a vowel");
+           
+      break;
+      case "e":
+          System.out.println("this is a vowel");
+      
+      break;
+      case "i":
+          System.out.println("this  is a vowel");
+      
+      break;
+      case "o":
+          System.out.println("this is vowel");
+      break;
+      case "u":
+          System.out.println("this is a vowel");
+      break;
+      default:
+          System.out.println("this is a not vowel");
+         }
+    }
+}
+       
+      
+          
+      
+      
+      
+    }
+
+   
+    }
+    
+}      case "a" -> System.out.println("this is a vowel");
+      case "e" -> System.out.println("this is a vowel");
+      case "i" -> System.out.println("this  is a vowel");
+      case "o" -> System.out.println("this is vowel");
+      case "u" -> System.out.println("this is a vowel");
+      default -> System.out.println("this is a not vowel");
+         }
+    }
+}
+       
+      
+          
+      
+      
+      
+    }
+
+   
+    }
+    
+}      case "a" -> System.out.println("this is a vowel");
+      case "e" -> System.out.println("this is a vowel");
+      case "i" -> System.out.println("this  is a vowel");
+      case "o" -> System.out.println("this is vowel");
+      case "u" -> System.out.println("this is a vowel");
+      default -> System.out.println("this is a not vowel");
+         }
+    }
+}
+       
+      
+          
+      
+      
+      
+    }
+
+   
+    }
+    
+}      case "a" -> System.out.println("this is a vowel");
+      case "e" -> System.out.println("this is a vowel");
+      case "i" -> System.out.println("this  is a vowel");
+      case "o" -> System.out.println("this is vowel");
+      case "u" -> System.out.println("this is a vowel");
+      default -> System.out.println("this is a not vowel");
+         }
+    }
+}
+       
+      
+          
+      
+      
+      
+    }
+
+   
+    }
+    
+}
